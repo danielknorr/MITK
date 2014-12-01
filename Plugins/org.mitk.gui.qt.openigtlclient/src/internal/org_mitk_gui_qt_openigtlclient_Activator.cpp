5b60b3b0898333a6f7b8c6a197806da99fc70342
Daniel Knorr <Daniel.Knorr@stud.hs-mannheim.de>
2014-12-01 15:54:47 +0100

    Fixed problems with th new module

--- Plugins/org.mitk.gui.qt.openigtlclient/src/internal/org_mitk_gui_qt_openigtlclient_Activator.cpp
+++ Plugins/org.mitk.gui.qt.openigtlclient/src/internal/org_mitk_gui_qt_openigtlclient_Activator.cpp
@@ -5,8 +5,14 @@
 ===================================================================*/
 
 #include "org_mitk_gui_qt_openigtlclient_Activator.h"
+#include "QmitkOpenIgtLinkClientView.h"
 
 #include <QtPlugin>
+
+#include <usGetModuleContext.h>
+#include <usModuleInitialization.h>
+
+US_INITIALIZE_MODULE("MitkOpenIGTLink", "MitkOpenIGTLink")
 
 namespace mitk {
 
@@ -21,7 +27,7 @@ void org_mitk_gui_qt_openigtlclient_Activator::start(ctkPluginContext* context)
 {
   this->m_Context = context;
 
-  BERRY_REGISTER_EXTENSION_CLASS(QmitkXnatTreeBrowserView, context)
+  BERRY_REGISTER_EXTENSION_CLASS(QmitkOpenIgtLinkClientView, context)
 }
 
 void org_mitk_gui_qt_openigtlclient_Activator::stop(ctkPluginContext* context)
