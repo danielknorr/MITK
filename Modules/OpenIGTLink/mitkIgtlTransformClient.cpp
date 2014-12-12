cceb5c193e711d519ade94c7ebe232ac86ac2562
Daniel Knorr <d.knorr@dkfz-heidelberg.de>
2014-12-12 16:40:19 +0100

    added receive method to transform client

--- Modules/OpenIGTLink/mitkIgtlTransformClient.cpp
+++ Modules/OpenIGTLink/mitkIgtlTransformClient.cpp
@@ -4,6 +4,8 @@
 
 #include "mitkIgtlTransformClient.h"
 
+#include "igtlOSUtil.h"
+
 namespace mitk {
 
 IgtlTransformClient::IgtlTransformClient()
@@ -16,6 +18,26 @@ IgtlTransformClient::~IgtlTransformClient()
 
 void IgtlTransformClient::Receive()
 {
+  // Allocate image Message Class
+  igtl::TransformMessage::Pointer transMsg;
+  transMsg = igtl::TransformMessage::New();
+  transMsg->SetMessageHeader(m_Header);
+  transMsg->AllocatePack();
+
+  // Receive transform data from the socket
+  socket->Receive(transMsg->GetPackBodyPointer(), transMsg->GetPackBodySize());
+
+  // Deserialize the transform data
+  // If you want to skip CRC check, call Unpack() without argument.
+  int c = transMsg->Unpack(1);
+
+  if (c & igtl::MessageHeader::UNPACK_BODY) // if CRC check is OK
+  {
+    // Retrive the transform data
+    igtl::Matrix4x4 matrix;
+    transMsg->GetMatrix(matrix);
+    // TODO
+  }
 }
 
 }  // end of namespace mitk
